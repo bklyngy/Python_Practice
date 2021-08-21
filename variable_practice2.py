fd6de73fb306ed1a7dd37b4b8526467d264cecd3
bklyngy <mattny@bellsouth.net>
2021-08-21 08:34:40 -0400

    Added upper()

--- variable_practice2.py
+++ variable_practice2.py
@@ -1,11 +1,12 @@
 first_answer = int(input("Please enter a number:\n"))
 second_answer = int(input("Please enter another number:\n"))
 choice = input("Enter 'A' for Add  or 'M' for Multiply':\n")
+choice = choice.upper()
 add_total = first_answer + second_answer
 mult_total = first_answer * second_answer
-if choice == 'A' or choice == 'a':
+if choice == 'A':
   print("Your answer is " + str(add_total))
-elif choice == 'M' or choice == 'm':
+elif choice == 'M':
   print("Your answer is " + str(mult_total))
 else:
   print("Invalid response")
